9048b678eb62f288c494634901e0cb053570872c
trwe-odoo <trwe@odoo.com>
2022-06-16 09:49:27 -0400

    Changed fields in @api.depends

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -24,7 +24,7 @@ class Session(models.Model):
 
     end_date = fields.Date(string='End Date', compute='_compute_end_date', inverse='_inverse_end_date', store=True)
 
-    @api.depends(start_date, end_date)
+    @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
             if not (record.start_date and record.duration):
