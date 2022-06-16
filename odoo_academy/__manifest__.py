aa7047893c70d11a04cc130d3593a674bc057884
trwe-odoo <trwe@odoo.com>
2022-06-16 09:33:34 -0400

    Added pivot view to courses and calendar view to sessions

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -13,7 +13,7 @@
     'website': 'https://www.odoo.com',
     'category': 'Training',
     'version': '0.1',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
