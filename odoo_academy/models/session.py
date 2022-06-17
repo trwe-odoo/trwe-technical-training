6c1804d3eb47c8c3fda31cfb7d28b04f6f6c8291
trwe-odoo <trwe@odoo.com>
2022-06-17 13:03:54 -0400

    Added sale wizard

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -24,6 +24,8 @@ class Session(models.Model):
 
     end_date = fields.Date(string='End Date', compute='_compute_end_date', inverse='_inverse_end_date', store=True)
 
+    total_price = fields.Float(string='Total Price', related='course_id.total_price')
+
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
